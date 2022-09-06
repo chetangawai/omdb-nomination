38cbf35ecc982ed46f07db1c68d4d0a6380de72d
Chetan Gawai <chetansgawai@gmail.com>
2022-09-06 16:32:58 +0530

    Removed unwanted code

--- tests/setupTests.js
+++ tests/setupTests.js
@@ -1,4 +1,5 @@
-import '@testing-library/jest-dom'
+import '@testing-library/jest-dom';
+
 jest.mock('./../src/config.js', () => {
   return { APP_CONFIG: { OMDB_API_KEY: 'OMDB_API_KEY' } };
 });
--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import EnvironmentPlugin from "vite-plugin-environment";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  //plugins: [react(), EnvironmentPlugin("all")],
-  plugins: [react({
-    fastRefresh: process.env.NODE_ENV !== 'test'
-  })]
-  // test: {
-  //   globals: true,
-  //   environment: 'jsdom',
-  //   setupFiles: './tests/setupTests.js',
-  // }
+  plugins: [react()]
 })
